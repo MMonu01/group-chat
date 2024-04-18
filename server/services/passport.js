45fe0089b002bee06cb1b2eb2a6d36fc8938830f
mohammad.monu <mohammad.monu@healthmug.com>
2024-04-18 22:25:37 +0530

    user data management

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,8 @@
 import passport from "passport";
+import generator from "generate-password";
 import { Strategy as GoogleStrategy } from "passport-google-oauth2";
 import "dotenv/config";
+import { userModel } from "../model/user-model.js";
 
 passport.use(
   new GoogleStrategy(
@@ -9,7 +11,19 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: `${process.env.PROJECT_URL}/auth/google/callback`,
     },
-    function (accessToken, refreshToken, profile, cb) {
+    async (accessToken, refreshToken, profile, cb) => {
+      const user = await userModel.findOne({ email: profile.email });
+      if (!user) {
+        const password = generator.generate({
+          length: 10,
+          numbers: true,
+          symbols: true,
+        });
+
+        const user_obj = { username: profile.displayName, email: profile.email, password, avatar: profile.picture };
+        const new_user = new userModel(user_obj);
+        await new_user.save();
+      }
       return cb(null, profile);
     }
   )

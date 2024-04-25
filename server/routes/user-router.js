d2e5027bced19828e541546aee6e8d1c7b35405b
mohammad.monu <mohammad.monu@healthmug.com>
2024-04-25 23:30:13 +0530

    some change

--- server/routes/user-router.js
+++ server/routes/user-router.js
@@ -6,14 +6,14 @@ export const userRouter = express.Router();
 
 userRouter.get("/userDetails", async (req, res, next) => {
   try {
-    if (!req.isAuthenticated()) {
-      res.send({ logged_in_success: false });
-    } else {
-      const { email } = req.user;
-      const user_details = await userModel.findOne({ email }, { username: 1, avatar: 1 });
-      const { _id, avatar, username } = user_details;
-      res.send({ logged_in_success: true, email, _id, avatar, username });
-    }
+    // if (!req.isAuthenticated()) {
+    res.send({ logged_in_success: false });
+    // } else {
+    //   const { email } = req.user;
+    //   const user_details = await userModel.findOne({ email }, { username: 1, avatar: 1 });
+    //   const { _id, avatar, username } = user_details;
+    //   res.send({ logged_in_success: true, email, _id, avatar, username });
+    // }
   } catch (err) {
     next(err);
   }
